22bd0a3d8770eee33c626fb71f2d937334547dbf
Ivan Tsybko <tsybko22@gmail.com>
2024-01-03 14:54:07 +0200

    add feature to fetch Open Trivia DB data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
+
 import AnswersSection from './components/AnswersSection';
 import QuestionSection from './components/QuestionScreen';
 
+import { getQuiz } from './utils/quizApi.js';
+
 const App = () => {
+  const [quiz, setQuiz] = useState({});
+
+  useEffect(() => {
+    getQuiz()
+      .then((quizData) => setQuiz(quizData))
+      .catch((err) => alert(err.message));
+  }, []);
+
   return (
-    <main className="min-h-screen grid grid-rows-2">
+    <main className="font-mono min-h-screen grid grid-rows-2">
       <QuestionSection />
       <AnswersSection />
     </main>
---
+++ src/utils/quizApi.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
+
+export const getQuiz = async () => {
+  const res = await fetch(BASE_URL);
+  const { results } = await res.json();
+
+  return results;
+};
