f27cfbb2ffd58e188c375285767904a5130dd855
Ivan Tsybko <tsybko22@gmail.com>
2024-01-03 14:54:38 +0200

    add feature to set random bg

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -3,8 +3,8 @@ import { randomHexColor } from '../utils/generateRandomHexColor';
 const QuestionSection = () => {
   return (
     <section
-      style={{ backgroundColor: randomHexColor }}
-      className="flex flex-col justify-center items-center gap-4"
+      style={{ background: randomHexColor }}
+      className="px-4 flex flex-col justify-center items-center gap-4"
     >
       <span className="text-xl text-zinc-800">1 &#8212; 10</span>
       <h1 className="font-serif text-5xl text-zinc-900">01. What is blablabla?</h1>
