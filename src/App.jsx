003b13b2455f75f21c3b9619d111bce6f7694986
Ivan Tsybko <tsybko22@gmail.com>
2024-01-04 13:22:11 +0200

    add feature to restart quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,73 @@
 import { useEffect, useState } from 'react';
 
 import AnswersSection from './components/AnswersSection';
+import Loader from './components/Loader.jsx';
 import QuestionSection from './components/QuestionSection';
+import ResultScreen from './components/ResultScreen.jsx';
 
 import { getQuiz } from './utils/quizApi.js';
 
 const App = () => {
   const [quiz, setQuiz] = useState([]);
   const [step, setStep] = useState(0);
+  const [correctCounter, setCorrectCounter] = useState(0);
   const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
-    getQuiz()
-      .then((quizData) => setQuiz(quizData))
-      .catch((err) => alert(err.message));
-  }, []);
-
-  const handleNextAnswer = () => {
-    if (step === quiz.length) {
-      setIsFinished(true);
-      setStep(0);
-      return;
+    if (!isFinished) {
+      getQuiz()
+        .then((quizData) => setQuiz(quizData))
+        .catch((err) => alert(err.message));
     }
-    setStep((answerCounter) => answerCounter + 1);
+  }, [isFinished]);
+
+  const handleRestartQuiz = () => {
+    setIsFinished(false);
+    setStep(0);
+    setCorrectCounter(0);
+    setQuiz([]);
   };
 
-  if (quiz.length === 0) return;
+  const handleAnswerClick = (answer) => {
+    setStep((prev) => prev + 1);
+
+    if (step === quiz.length - 1) {
+      setIsFinished(true);
+    }
+
+    if (answer === quiz[step].correct_answer) {
+      setCorrectCounter((prev) => prev + 1);
+    }
+  };
+
+  if (quiz.length === 0) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-zinc-900">
+        <Loader />
+      </div>
+    );
+  }
 
   return (
     <main className="font-mono bg-zinc-900">
-      <div className="min-h-screen grid grid-col-2">
-        <QuestionSection
-          quiz={quiz}
-          step={step}
+      {isFinished ? (
+        <ResultScreen
+          correctCounter={correctCounter}
+          handleRestartQuiz={handleRestartQuiz}
         />
-        <AnswersSection />
-      </div>
+      ) : (
+        <div className="min-h-screen grid grid-col-2">
+          <QuestionSection
+            quiz={quiz}
+            step={step}
+          />
+          <AnswersSection
+            correctAnswer={quiz[step].correct_answer}
+            incorrectAnswers={quiz[step].incorrect_answers}
+            handleAnswerClick={handleAnswerClick}
+          />
+        </div>
+      )}
     </main>
   );
 };
