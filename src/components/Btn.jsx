afcc72369e8ec6bc89901d32a4797233beeeb34d
Ivan Tsybko <tsybko22@gmail.com>
2024-01-04 13:13:15 +0200

    add className prop to Btn component

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,7 +1,7 @@
-const Btn = ({ children, ...props }) => {
+const Btn = ({ children, className, ...props }) => {
   return (
     <button
-      className="px-10 py-3 rounded-full transition-colors text-2xl text-zinc-300 bg-zinc-800 hover:bg-zinc-700"
+      className={`px-10 py-3 rounded-full transition-colors text-2xl text-zinc-300 bg-zinc-800 hover:bg-zinc-700 ${className}`}
       {...props}
     >
       {children}
