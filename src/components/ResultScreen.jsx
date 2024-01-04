880625184e43ce219d565dbb91af69baa5a822a9
Ivan Tsybko <tsybko22@gmail.com>
2024-01-04 13:12:47 +0200

    add ResultScreen component

---
+++ src/components/ResultScreen.jsx
@@ -0,0 +1,17 @@
+import Btn from './Btn';
+
+const ResultScreen = ({ correctCounter, handleRestartQuiz }) => {
+  return (
+    <section className="min-h-screen flex flex-col gap-6 items-center justify-center">
+      <h1 className="text-5xl text-zinc-300">
+        Your result is:{' '}
+        <span className={`${correctCounter > 5 ? 'text-green-400' : 'text-red-400'}`}>
+          {correctCounter}
+        </span>
+      </h1>
+      <Btn onClick={handleRestartQuiz}>Restart Quiz!</Btn>
+    </section>
+  );
+};
+
+export default ResultScreen;
