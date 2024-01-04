2d740755b328abb347a51a6dbc5e2139c65c93fd
Ivan Tsybko <tsybko22@gmail.com>
2024-01-04 13:14:52 +0200

    add color transition styles

--- src/components/QuestionSection.jsx
+++ src/components/QuestionSection.jsx
@@ -1,17 +1,17 @@
 import { randomHexColor } from '../utils/generateRandomHexColor';
 
 const QuestionSection = ({ quiz, step }) => {
-  const currentQuestion = quiz[step].question.replace(/&quot;/g, '"');
+  const currentQuestion = quiz[step].question;
 
   return (
     <section
-      style={{ background: randomHexColor }}
-      className="px-4 flex flex-col justify-center items-center gap-4"
+      style={{ background: randomHexColor() }}
+      className="px-4 flex flex-col justify-center items-center gap-4 transition-colors"
     >
       <span className="text-xl text-zinc-800">
         {step + 1} &#8212; {quiz.length}
       </span>
-      <h1 className="font-serif text-5xl text-zinc-900">
+      <h1 className="font-serif text-5xl text-zinc-900 text-center">
         0{step + 1}. {currentQuestion}
       </h1>
     </section>
