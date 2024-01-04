f18220549023f92581bfefa524d684fe201a7700
Ivan Tsybko <tsybko22@gmail.com>
2024-01-04 13:14:13 +0200

    convert to arrow function

--- src/utils/generateRandomHexColor.js
+++ src/utils/generateRandomHexColor.js
@@ -1,1 +1,2 @@
-export const randomHexColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+export const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
